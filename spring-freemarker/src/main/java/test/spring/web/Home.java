ab92e7dc36781a523a5430815b28c50fb8fd8392
Trympyrym <rio09876@gmail.com>
2017-01-11 14:55:44 +0200

    homework

--- spring-freemarker/src/main/java/test/spring/web/Home.java
+++ spring-freemarker/src/main/java/test/spring/web/Home.java
@@ -5,14 +5,62 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import test.spring.Database;
+import test.spring.DatabaseRecord;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Home {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+    private static Database database = new Database();
+    private static String qwer = "qw";
+    static
+    {
+        database.add(new DatabaseRecord("first task"));
+        database.add(new DatabaseRecord("second task"));
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String redirectToIndex()
+    {
+        return "redirect:index.html";
+    }
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String init(@ModelAttribute("model") ModelMap model) {
 		model.addAttribute("name", "Test name");
-		model.addAttribute("comment", "Test comment");
+		model.addAttribute("comment", qwer);
 
+		model.addAttribute("tasks", database.getAll());
 		return "index";
 	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(String newTaskName)
+    {
+        database.add(new DatabaseRecord(newTaskName));
+        return "redirect:index.html";
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    public String remove(String index)
+    {
+        database.remove(castFreemarkerInputToInt(index));
+
+        return "redirect:index.html";
+    }
+
+    @RequestMapping(value = "/changeDoneMark", method = RequestMethod.POST)
+    public String changeDoneMark(String index)
+    {
+        database.changeDoneMark(castFreemarkerInputToInt(index));
+
+        return "redirect:index.html";
+    }
+
+    private Integer castFreemarkerInputToInt(String argString)
+    {
+        return Integer.parseInt(argString.substring(0, argString.length() - 1));
+    }
 }
